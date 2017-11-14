007896d01e5837828fe64c51c416c7562c04fe55
Laercio Leal <laercio.leal@zup.com.br>
2017-11-13 23:51:00 -0200

    added animation when the cardSelection is touched

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { Text,
+        StyleSheet,
+        TouchableWithoutFeedback,
+        View,
+        LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
 
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { library, expanded } = this.props;
 
     if(expanded){
       return(
-        <Text>{library.description}</Text>
+        <CardSection style={{ flex: 1 }}>
+          <Text>{library.description}</Text>
+        </CardSection>
       );
     }
   }
