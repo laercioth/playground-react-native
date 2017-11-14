89a77f028749bd57beef2dc8fd1f0a8e66a1ab19
Laercio Leal <laercio.leal@zup.com.br>
2017-11-13 23:29:50 -0200

    fixing gitignore

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -7,9 +7,9 @@ import * as actions from '../actions';
 class ListItem extends Component {
 
   renderDescription() {
-    const { library, selectedLibraryId } = this.props;
+    const { library, expanded } = this.props;
 
-    if(library.id === selectedLibraryId){
+    if(expanded){
       return(
         <Text>{library.description}</Text>
       );
@@ -45,8 +45,10 @@ const styles = StyleSheet.create({
   }
 });
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId};
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
