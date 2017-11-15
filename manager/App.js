67b51f15dd0cfbbe4564085838c1df404d8ee0f4
Laercio Leal <laercio.leal@zup.com.br>
2017-11-15 16:36:56 -0200

    made  more some implementation about of login and added components and some configs

--- manager/App.js
+++ manager/App.js
@@ -3,13 +3,29 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './src/reducers';
+import firebase from 'firebase';
+import LoginForm from './src/components/LoginForm';
 
 export default class App extends Component<{}> {
+
+  componetWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAXW7DgQGeEc_XB2RWQpeSffpjQEvaeZNI',
+      authDomain: 'manager-b50d5.firebaseapp.com',
+      databaseURL: 'https://manager-b50d5.firebaseio.com',
+      projectId: 'manager-b50d5',
+      storageBucket: 'manager-b50d5.appspot.com',
+      messagingSenderId: '462436493651'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(reducers)}>
         <View>
-          Hello!
+          <LoginForm />
         </View>
       </Provider>
     );
---
+++ manager/src/components/common/CardSection.js
@@ -0,0 +1,27 @@
+// Import libraries for making a component
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const CardSection = (prop) => {
+  return (
+    <View style={styles.container}>
+      {prop.children}
+    </View>
+  );
+};
+
+// StyleSheet
+const styles = StyleSheet.create({
+	container: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#FFF',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#DDD',
+    position: 'relative'
+  }
+});
+
+// Make the component available to other parts of the app
+export { CardSection };
