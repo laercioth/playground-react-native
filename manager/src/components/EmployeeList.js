0bf60f89b18d0688547410104c4a8eadb4e4e0ab
Laercio Leal <laercio.leal@zup.com.br>
2017-11-18 00:51:55 -0200

    finished find and show on ListView the data.

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { ListView, View, Text } from "react-native";
 import { connect } from "react-redux";
 import { employeesFetch } from "../actions";
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
@@ -25,12 +26,20 @@ class EmployeeList extends Component {
     this.dataSource = ds.cloneWithRows(employees);
   }
 
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
+
   render() {
-    console.log(this.props)
+
     return (
-      <View>
-        <Text>asdadasd</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+
+      />
     );
   }
 }
