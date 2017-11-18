ae9656e55f90dc83f85343863c172ad816d21390
Laercio Leal <laercio.leal@zup.com.br>
2017-11-18 00:40:13 -0200

    implemented find by id and foreach by component. installed lodash

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,24 +1,46 @@
+import _ from 'lodash';
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ListView, View, Text } from "react-native";
 import { connect } from "react-redux";
 import { employeesFetch } from "../actions";
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  // nextProps are the next set of props that this componentWillMount
+  //will be rendered cloneWithRows
+  //this.props is still the old set of props
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 != r2
+    });
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
+    console.log(this.props)
     return (
       <View>
-        <Text>Teste</Text>
-        <Text>Teste</Text>
-        <Text>Teste</Text>
-        <Text>Teste</Text>
-        <Text>Teste</Text>
+        <Text>asdadasd</Text>
       </View>
     );
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
