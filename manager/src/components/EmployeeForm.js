e5b2150eeb3237b33ea1684cb995cc73b13eb61e
Laercio Leal <laercio.leal@zup.com.br>
2017-11-18 23:52:13 -0200

    refactored stuffs about of creation employees.

---
+++ manager/src/components/EmployeeForm.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { View, Text, Picker, StyleSheet } from "react-native";
+import { CardSection, Input } from "./common";
+import { connect } from "react-redux";
+import { employeeUpdate } from "../actions";
+
+class EmployeeForm extends Component {
+  render() {
+    return (
+      <View>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jhon"
+            value={this.props.name}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "name", value })
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="(34) 90000-0000"
+            value={this.props.phone}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "phone", value })
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+          <Text style={styles.pickerStyle}>Shift</Text>
+        </CardSection>
+
+        <CardSection>
+          <Picker
+            style={{ flex: 1 }}
+            selectedValue={this.props.shift}
+            onValueChange={value =>
+              this.props.employeeUpdate({ prop: "shift", value })
+            }
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+      </View>
+    );
+  }
+}
+
+// StyleSheet
+const styles = StyleSheet.create({
+  pickerStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    marginBottom: 5,
+    color: "black"
+  }
+});
+
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(null, { employeeUpdate })(EmployeeForm);
