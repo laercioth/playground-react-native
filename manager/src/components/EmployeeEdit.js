b241a94f86d48113b11394c3d3aae618a40e165b
Laercio Leal <laercio.leal@zup.com.br>
2017-11-19 01:05:10 -0200

    made edition to save on db

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,40 +1,38 @@
-
-import _ from 'lodash';
-import React, { Component } from 'react';
-import EmployeeForm from './EmployeeForm';
-import { connect } from 'react-redux';
-import { Card, CardSection, Button } from './common';
-import { employeeUpdate } from '../actions';
+import _ from "lodash";
+import React, { Component } from "react";
+import EmployeeForm from "./EmployeeForm";
+import { connect } from "react-redux";
+import { Card, CardSection, Button } from "./common";
+import { employeeUpdate, employeeSave } from "../actions";
 
 class EmployeeEdit extends Component {
-
   componentWillMount() {
-    _.each(this.props.employee, (value, prop ) => {
+    _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
     });
   }
   onButtonPress() {
     const { name, phone, shift } = this.props;
-    console.log(name, phone, shift);
+    this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
 
   render() {
-    return(
+    return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Save Changes
-          </Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
         </CardSection>
       </Card>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const { name, phone, shift } = state.employeeForm;
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(
+  EmployeeEdit
+);
