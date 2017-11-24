1b2c10cb18362cdf38cc2b687c270961988c0f00
Laercio Leal <laercio.leal@zup.com.br>
2017-11-23 22:34:32 -0200

    added several dependencies and making the layout of search weather

--- weather/App.js
+++ weather/App.js
@@ -5,23 +5,44 @@
  */
 import React, { Component } from "react";
 import { View, Text, Image } from "react-native";
-import { Header } from "./src/components/common";
 import WeatherTop from "./src/components/WeatherTop";
 import WeatherMid from "./src/components/WeatherMid";
 import WeatherBottom from "./src/components/WeatherBottom";
 import GooglePlaceAutocomplete from "react-native-google-place-autocomplete";
+import { Header, SearchBar } from "react-native-elements";
+import { Col, Row, Grid } from "react-native-easy-grid";
 
 export default class App extends Component {
-  state = { result: ''}
+  state = { result: "" };
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Header headerText={"Weather"} />
-        <GooglePlaceAutocomplete
-          googleAPIKey="AIzaSyDN3dEHSb6IWD9uuQBRlt43K6_A0FpLAeg"
-          onResult={result => console.log}
-          placeholder="Type to search..."
+        <Header
+          statusBarProps={{ barStyle: "light-content" }}
+          centerComponent={{ text: "Weather", style: { color: "#fff" } }}
+          outerContainerStyles={{ backgroundColor: "#3D6DCC" }}
+          innerContainerStyles={{ justifyContent: "space-around" }}
         />
+        <SearchBar lightTheme placeholder="Typing the City ..." />
+
+        <Grid>
+          <Col>
+            <Row style={{ backgroundColor: "yellow" }}>
+              <Text>1</Text>
+            </Row>
+            <Row style={{ backgroundColor: "gray" }}>
+              <Text>2</Text>
+            </Row>
+          </Col>
+          <Col>
+            <Row style={{ backgroundColor: "blue" }}>
+              <Text>3</Text>
+            </Row>
+            <Row style={{ backgroundColor: "white" }}>
+              <Text>4</Text>
+            </Row>
+          </Col>
+        </Grid>
       </View>
     );
   }
